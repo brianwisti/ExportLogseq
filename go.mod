64dad253f20ef16bdf9039e0fe24059225f8e9ea
Brian Wisti <brianwisti@pobox.com>
2024-06-28 21:24:32 -0700

    feat: parse logse config

--- go.mod
+++ go.mod
@@ -2,4 +2,14 @@ module export-logseq
 
 go 1.22.4
 
-require github.com/lpernett/godotenv v0.0.0-20230527005122-0de1d4c5ef5e
+require (
+	github.com/lpernett/godotenv v0.0.0-20230527005122-0de1d4c5ef5e
+	github.com/nleeper/goment v1.4.4
+	github.com/sirupsen/logrus v1.9.3
+	olympos.io/encoding/edn v0.0.0-20201019073823-d3554ca0b0a3
+)
+
+require (
+	github.com/tkuchiki/go-timezone v0.2.0 // indirect
+	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
+)
--- main.go
+++ main.go
@@ -1,14 +1,78 @@
 package main
 
 import (
-  "log"
-  "os"
+	"fmt"
+	"os"
+	"path/filepath"
 
-  "github.com/lpernett/godotenv"
+	"github.com/lpernett/godotenv"
+	log "github.com/sirupsen/logrus"
+	"olympos.io/encoding/edn"
 )
 
+// Describes the aspects of Logseq configuration we are interested in
+type LogseqConfig struct {
+	FileNameFormat  edn.Keyword `edn:"file/name-format"`
+	PreferredFormat edn.Keyword `edn:"preferred-format"`
+}
+
+func LoadConfig(configFile string) (LogseqConfig, error) {
+	log.Info("Loading config file:", configFile)
+	logseqConfigBytes, err := os.ReadFile(configFile)
+	if err != nil {
+		return LogseqConfig{}, fmt.Errorf("reading config file: %v", err)
+	}
+
+	var f LogseqConfig
+	ednErr := edn.Unmarshal(logseqConfigBytes, &f)
+	if ednErr != nil {
+		log.Fatalf("Error unmarshalling EDN: %v", ednErr)
+		return LogseqConfig{}, fmt.Errorf("unmarshalling EDN: %v", ednErr)
+	}
+
+	log.Info("Unmarshalled EDN", f)
+
+	if f.FileNameFormat != "triple-lowbar" {
+		return f, fmt.Errorf("unsupported file/name-format: %v", f.FileNameFormat)
+	}
+
+	if f.PreferredFormat != "markdown" {
+		return f, fmt.Errorf("unsupported preferred-format: %v", f.PreferredFormat)
+	}
+
+	return f, nil
+}
+
 func main() {
-  godotenv.Load()
-  graphDir := os.Getenv("GRAPH_DIR")
-  log.Println("GRAPH_DIR:", graphDir)
+	log.Info("Initializing...")
+	godotenv.Load()
+	graphDir := os.Getenv("GRAPH_DIR")
+	log.Info("GRAPH_DIR:", graphDir)
+	if graphDir == "" {
+		log.Fatal("GRAPH_DIR is not set in .env file or environment variables")
+	}
+	configFile := filepath.Join(graphDir, "logseq", "config.edn")
+
+	logseqConfigBytes, err := os.ReadFile(configFile)
+	if err != nil {
+		log.Fatal("Error reading file:", err)
+	}
+	var f LogseqConfig
+	ednErr := edn.Unmarshal(logseqConfigBytes, &f)
+	if ednErr != nil {
+		log.Fatalf("Error unmarshalling EDN: %v", ednErr)
+	}
+	log.Info("Unmarshalled EDN", f)
+	log.Info("File Name Format:", f.FileNameFormat)
+
+	// We're specifically catering to my graph first.
+	if f.FileNameFormat != "triple-lowbar" {
+		log.Fatal("Unsupported file name format:", f.FileNameFormat)
+	}
+
+	if f.PreferredFormat != "markdown" {
+		log.Fatal("Unsupported preferred format:", f.PreferredFormat)
+	}
+
+	log.Info("All done!")
 }
